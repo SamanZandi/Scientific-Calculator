a4a84fd7fe424a55060bbdb817d3a915cafaf092
Saman <saman_zandi91@yahoo.com>
2024-12-02 12:01:42 +0330

    "Calculator commit" after 3rd edited calculation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.zandroid.mycalculator"
         minSdk = 23
         targetSdk = 34
-        versionCode = 11
-        versionName = "4.0"
+        versionCode = 12
+        versionName = "4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
